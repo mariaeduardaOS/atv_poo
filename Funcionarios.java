666ed88194538f68e7e47009ff400ec12c3d9f4e
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-05-15 10:37:35 -0300

    Create Funcionarios.java

---
+++ Funcionarios.java
@@ -0,0 +1,16 @@
+public class Funcionarios{
+    public String nome;
+    private double valorhora;
+    public int Cod_empresa;
+    public int horatrabmes;
+
+    public Funcionarios(String nome, double valorhora, int cod_empresa, int horatrabmes) {
+        this.nome = nome;
+        this.valorhora = valorhora;
+        this.Cod_empresa = cod_empresa;
+        this.horatrabmes = horatrabmes;
+    }
+    public double calcsal(){
+        return valorhora * horatrab;
+    }
+}
