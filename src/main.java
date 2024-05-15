95040fb8bf25c03d03d1aa27e07e9547cdaa1603
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-05-15 11:40:17 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,8 +4,8 @@ public class Main {
      Gerente gerente = new Gerente("Lucas", 22, 10141, 120);
      Lider lider = new Lider("João", 19, 10142, 110);
 
-        System.out.println(funcionarios.calcsal());
-        System.out.println(gerente.calcsal());
-        System.out.println(lider.calcsal());
+        System.out.println(funcionarios.calcsalario());
+        System.out.println(gerente.calcsalario());
+        System.out.println(lider.calcsalario());
     }
 }
