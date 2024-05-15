532f1c1253db43dcb4c6dc735e8a07a7063be581
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-05-15 10:41:03 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+     Funcionarios funcionarios = new Funcionarios("Mateus", 12, 10140, 100);
+     Gerente gerente = new Gerente("Lucas", 22, 10141, 120);
+     Lider lider = new Lider("João", 19, 10142, 110);
+
+        System.out.println(funcionarios.calcsal());
+        System.out.println(gerente.calcsal());
+        System.out.println(lider.calcsal());
+    }
+}
